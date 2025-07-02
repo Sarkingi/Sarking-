958a1ef2f3b957cabba25c98764e499be7173c6f
Sarkingi <abubakaraminusadauki6868@gmail.com>
2025-07-02 18:47:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
